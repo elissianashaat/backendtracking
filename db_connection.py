7c15deacb2976a2b9bdf988ffa4de42af4cc7504
amr mahmoud <94997694+AmrMahmoudSaid@users.noreply.github.com>
2024-12-24 06:19:32 +0200

    Update db_connection.py

--- db_connection.py
+++ db_connection.py
@@ -3,7 +3,7 @@ import os
 import pymongo
 
 # url = 'mongodb://localhost:27017/'
-url = 'mongodb://admin:password@mongodb-service:27017/appdb'
+url = 'mongodb://admin:password@mongodb-srv:27017/appdb'
 # url = os.environ.get('MONGO_URL')
 
 client = pymongo.MongoClient(url)
